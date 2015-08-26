c8e79725d0e7feb9fe8da67e5803d25accad2da4
RobertAKARobin <robertgfthomas@gmail.com>
2015-08-26 12:52:55 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var passport = require('passport');
+var Strategy = require('passport-twitter').Strategy;
+var env = require("./env");
+
+passport.use(new Strategy(
+  {
+    consumerKey: env.consumerKey,
+    consumerSecret: env.consumerSecret,
+    callbackURL: env.callbackUrl
+  },
+  function(token, tokenSecret, profile, cb){
+    return cb(null, profile);
+  }
+));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+var app = express();
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/',
+  function(req, res) {
+    res.json(req.session);
+  });
+
+app.get('/login',
+  function(req, res){
+    res.send("Log in");
+  });
+
+app.get('/login/twitter', passport.authenticate('twitter'));
+
+app.get('/auth/twitter/callback',
+  passport.authenticate('twitter', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
+
+app.get('/profile',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function(req, res){
+    res.render('profile', { user: req.user });
+  }
+);
+
+app.listen(3001);
